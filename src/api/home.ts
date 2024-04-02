a7d11d4cce695afe3d1445989e20d8990a46b7ff
zhangyulin <yulin.zhang@camtg.cn>
2024-04-02 10:53:25 +0800

    feat: 新鲜好物

--- src/api/home.ts
+++ src/api/home.ts
@@ -3,3 +3,13 @@ import httpInstance from '@/utils/http'
 export const getBannerAPI = () => {
   return httpInstance.get('home/banner')
 }
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = (): any => {
+  return httpInstance({
+    url: '/home/new',
+  })
+}
