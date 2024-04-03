b97ddc2336e094a288729868ac779dfec1d367f9
zhangyulin <yulin.zhang@camtg.cn>
2024-04-03 10:19:23 +0800

    feat: 面包屑导航

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 // createRouter：创建router实例对象
 // createWebHistory：创建history模式的路由
-
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +22,10 @@ const router = createRouter({
         {
           path: 'category/:id',
           component: Category,
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory,
         },
       ],
     },
