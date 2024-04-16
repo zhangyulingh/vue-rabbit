969aee5aeefaad2675f9eb62b08b42f35088f196
zhangyulin <yulin.zhang@camtg.cn>
2024-04-16 14:45:05 +0800

    feat: 数量和价格

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -17,7 +17,13 @@ export const useCartStore = defineStore(
       const idx = cartList.value.findIndex(item => item.skuId === goods.skuId)
       cartList.value.splice(idx, 1)
     }
-    return { cartList, addCart, delCart }
+    const allCount = computed(() =>
+      cartList.value.reduce((a, c) => a + c.count, 0),
+    )
+    const allPrice = computed(() =>
+      cartList.value.reduce((a, c) => a + c.count * c.price, 0),
+    )
+    return { cartList, addCart, delCart, allCount, allPrice }
   },
   {
     persist: true,
