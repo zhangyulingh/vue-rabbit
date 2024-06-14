555fb6d23ed10e07106164e05c0a957f807f5410
zhangyulin <yulin.zhang@camtg.cn>
2024-06-14 14:57:29 +0800

    feat: update

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,5 +1,15 @@
 <script setup>
-const payInfo = {}
+import { getOrderAPI } from '@/api/pay'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const payInfo = ref({})
+const getPayInfo = async () => {
+  const res = await getOrderAPI(route.query.id)
+  payInfo.value = res.result
+}
+onMounted(() => {
+  getPayInfo()
+})
 </script>
 
 <template>
