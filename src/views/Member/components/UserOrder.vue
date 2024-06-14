a368013f12e236d6b6ab2327e82e77150a698d80
zhangyulin <yulin.zhang@camtg.cn>
2024-06-14 17:43:39 +0800

    feat: update

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -13,6 +13,7 @@ const tabTypes = [
 // 订单列表
 // 获取订单列表
 const orderList = ref([])
+const total = ref(0)
 const params = ref({
   orderState: 0,
   page: 1,
@@ -21,10 +22,15 @@ const params = ref({
 const getOrderList = async () => {
   const res = await getUserOrder(params.value)
   orderList.value = res.result.items
+  total.value = res.result.counts
 }
 onMounted(() => getOrderList())
 const tabChange = type => {
   params.value.orderState = type
+  getOrderList()
+}
+const pageChange = page => {
+  params.value.page = page
   getOrderList()
 }
 </script>
@@ -122,7 +128,13 @@ const tabChange = type => {
           </div>
           <!-- 分页 -->
           <div class="pagination-container">
-            <el-pagination background layout="prev, pager, next" />
+            <el-pagination
+              background
+              layout="prev, pager, next"
+              :total="total"
+              :page-size="pageSize"
+              @current-change="pageChange"
+            />
           </div>
         </div>
       </div>
